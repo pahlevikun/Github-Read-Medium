4a9a1d1f9fccd712657b233dff0f02dc31402a9c
Farhan Yuda Pahlevi <pahlevi.kun@gmail.com>
2020-12-19 00:34:11 +0700

    Update package

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 app.use(express.json());
-app.use(require("./src/route/router"))
+app.use(require("./route/router"))
 
 app.listen(port, function () {
   console.log("Server listening " + port);
